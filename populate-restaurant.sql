68380883c4b47cd75be315a546912b570dc4b114
kkey1 <kkey1@villanova.edu>
2020-11-10 23:17:20 -0500

    Waiter and Host Data added

---
+++ populate-restaurant.sql
@@ -0,0 +1,350 @@
+INSERT INTO restaurant VALUES
+('150 Central Park','1','10:00am-12:00am','15');
+INSERT INTO restaurant VALUES
+('Chops Grille','2','10:00am-12:00am','25');
+INSERT INTO restaurant VALUES
+('Giovanni''s Table','3','10:00am-12:00am','20');
+INSERT INTO restaurant VALUES
+('Izumi Hibachi ' ||chr(38)|| ' Sushi','4','10:00am-12:00am','15');
+
+INSERT INTO waiter VALUES
+('Salvatore','Brooks','610951626','1');
+INSERT INTO waiter VALUES
+('Blanca','Arnold','496443955','1');
+INSERT INTO waiter VALUES
+('Andrew','Newton','106990635','1');
+INSERT INTO waiter VALUES
+('Levi','Watkins','234941254','1');
+INSERT INTO waiter VALUES
+('Daniel','Page','380628740','1');
+INSERT INTO waiter VALUES
+('Gordon','Perez','649928414','1');
+INSERT INTO waiter VALUES
+('Dallas','Schwartz','113168875','1');
+INSERT INTO waiter VALUES
+('Marsha','Woods','910781920','1');
+INSERT INTO waiter VALUES
+('Bernadette','Sullivan','941373596','1');
+INSERT INTO waiter VALUES
+('Georgia','Waters','738480675','1');
+INSERT INTO waiter VALUES
+('Connie','Shaw','376363455','1');
+INSERT INTO waiter VALUES
+('Gerald','Hart','566538004','1');
+INSERT INTO waiter VALUES
+('Inez','Fox','271547370','1');
+INSERT INTO waiter VALUES
+('Patsy','Williamson','220355904','1');
+INSERT INTO waiter VALUES
+('Luther','Stevenson','559086787','1');
+INSERT INTO waiter VALUES
+('Martin','Peters','014279718','1');
+INSERT INTO waiter VALUES
+('Yvonne','Walton','923191059','1');
+INSERT INTO waiter VALUES
+('Brandy','Holloway','106877608','1');
+INSERT INTO waiter VALUES
+('Santiago','Park','294149400','1');
+INSERT INTO waiter VALUES
+('Irving','Gonzalez','177362271','1');
+INSERT INTO waiter VALUES
+('Monica','Fleming','644251857','1');
+INSERT INTO waiter VALUES
+('Bert','Klein','344735295','1');
+INSERT INTO waiter VALUES
+('Sherman','Conner','936961688','1');
+INSERT INTO waiter VALUES
+('Joe','Kim','654615221','1');
+INSERT INTO waiter VALUES
+('Angelina','Harrison','980478363','1');
+INSERT INTO waiter VALUES
+('Matthew','Hines','530329460','1');
+INSERT INTO waiter VALUES
+('Tabitha','Spencer','823871363','1');
+INSERT INTO waiter VALUES
+('Sergio','Mccoy','497622936','1');
+INSERT INTO waiter VALUES
+('Adam','Ingram','553583730','1');
+INSERT INTO waiter VALUES
+('Ellen','Burke','637379803','1');
+INSERT INTO waiter VALUES
+('Edith','Tran','763097326','2');
+INSERT INTO waiter VALUES
+('Arturo','Stevens','389790097','2');
+INSERT INTO waiter VALUES
+('Glenda','Pierce','719187559','2');
+INSERT INTO waiter VALUES
+('Darren','Weber','428866429','2');
+INSERT INTO waiter VALUES
+('Oscar','Boyd','188923740','2');
+INSERT INTO waiter VALUES
+('Melba','Wolfe','883516597','2');
+INSERT INTO waiter VALUES
+('Otis','Gutierrez','755386053','2');
+INSERT INTO waiter VALUES
+('Marcus','Ward','389573576','2');
+INSERT INTO waiter VALUES
+('Marc','Paul','698275771','2');
+INSERT INTO waiter VALUES
+('Archie','Crawford','702623399','2');
+INSERT INTO waiter VALUES
+('Anna','Francis','087510269','2');
+INSERT INTO waiter VALUES
+('Salvador','Ortega','530847557','2');
+INSERT INTO waiter VALUES
+('Regina','Freeman','848433641','2');
+INSERT INTO waiter VALUES
+('Gwen','Cook','392933517','2');
+INSERT INTO waiter VALUES
+('Roger','Wright','979450154','2');
+INSERT INTO waiter VALUES
+('Garry','Nunez','099999774','2');
+INSERT INTO waiter VALUES
+('Melissa','Harrington','204922703','2');
+INSERT INTO waiter VALUES
+('Krystal','Garza','403514862','2');
+INSERT INTO waiter VALUES
+('Troy','Powers','675138259','2');
+INSERT INTO waiter VALUES
+('Emily','Lyons','373420391','2');
+INSERT INTO waiter VALUES
+('Tim','Davis','371778322','2');
+INSERT INTO waiter VALUES
+('Lauren','Webster','497297008','2');
+INSERT INTO waiter VALUES
+('Kelly','Cook','045893726','2');
+INSERT INTO waiter VALUES
+('Tammy','King','302172621','2');
+INSERT INTO waiter VALUES
+('Tricia','Jacobs','375490159','2');
+INSERT INTO waiter VALUES
+('Tasha','Adkins','736501864','2');
+INSERT INTO waiter VALUES
+('Nichole','Berry','621891684','2');
+INSERT INTO waiter VALUES
+('Simon','Morris','625754045','2');
+INSERT INTO waiter VALUES
+('Eloise','Reyes','820527340','2');
+INSERT INTO waiter VALUES
+('Karen','Maxwell','643942793','2');
+INSERT INTO waiter VALUES
+('Bradford','Quinn','333238328','2');
+INSERT INTO waiter VALUES
+('Ruby','Andrews','667728536','2');
+INSERT INTO waiter VALUES
+('Suzanna','Lucas','561816142','2');
+INSERT INTO waiter VALUES
+('Alicia','Sanchez','678205626','2');
+INSERT INTO waiter VALUES
+('Madeline','Hicks','429622260','2');
+INSERT INTO waiter VALUES
+('William','Park','599298802','2');
+INSERT INTO waiter VALUES
+('Carla','Snyder','520992111','2');
+INSERT INTO waiter VALUES
+('Michele','Hines','955722366','2');
+INSERT INTO waiter VALUES
+('Tonya','Hudson','566362337','2');
+INSERT INTO waiter VALUES
+('Virginia','Peterson','141794252','2');
+INSERT INTO waiter VALUES
+('Dianna','Swanson','265545313','2');
+INSERT INTO waiter VALUES
+('Maggie','Miller','981219857','2');
+INSERT INTO waiter VALUES
+('Alexander','Copeland','809229921','2');
+INSERT INTO waiter VALUES
+('Megan','Burgess','941452922','2');
+INSERT INTO waiter VALUES
+('Lynn','Tucker','518209593','2');
+INSERT INTO waiter VALUES
+('Tracy','Washington','275631721','2');
+INSERT INTO waiter VALUES
+('Roman','Stevenson','384098131','2');
+INSERT INTO waiter VALUES
+('Margarita','Lopez','586524454','2');
+INSERT INTO waiter VALUES
+('Alberto','Kennedy','696383921','2');
+INSERT INTO waiter VALUES
+('Grant','Fernandez','654098240','2');
+INSERT INTO waiter VALUES
+('Arnold','Love','990653793','3');
+INSERT INTO waiter VALUES
+('Wayne','Estrada','435285477','3');
+INSERT INTO waiter VALUES
+('Shawna','Carroll','486540843','3');
+INSERT INTO waiter VALUES
+('May','Tate','007322512','3');
+INSERT INTO waiter VALUES
+('Winifred','Coleman','367478363','3');
+INSERT INTO waiter VALUES
+('Ricardo','Vargas','199143524','3');
+INSERT INTO waiter VALUES
+('Pam','Walters','801656354','3');
+INSERT INTO waiter VALUES
+('Lucille','Wallace','576780663','3');
+INSERT INTO waiter VALUES
+('Stephen','Nichols','580038904','3');
+INSERT INTO waiter VALUES
+('Jenna','Wolfe','740241587','3');
+INSERT INTO waiter VALUES
+('Francisco','Castillo','278269343','3');
+INSERT INTO waiter VALUES
+('Johnny','Alvarez','611368229','3');
+INSERT INTO waiter VALUES
+('Kellie','Erickson','300221215','3');
+INSERT INTO waiter VALUES
+('Wilson','Bush','270318331','3');
+INSERT INTO waiter VALUES
+('Johanna','Potter','402123184','3');
+INSERT INTO waiter VALUES
+('Christine','Brooks','229528710','3');
+INSERT INTO waiter VALUES
+('Bryant','Gregory','276121294','3');
+INSERT INTO waiter VALUES
+('Ricky','Patton','593510178','3');
+INSERT INTO waiter VALUES
+('Nathan','Brewer','308672638','3');
+INSERT INTO waiter VALUES
+('Elijah','Hicks','580605218','3');
+INSERT INTO waiter VALUES
+('Bennie','Hudson','790896292','3');
+INSERT INTO waiter VALUES
+('Irvin','Vega','539062233','3');
+INSERT INTO waiter VALUES
+('Malcolm','Barrett','838248760','3');
+INSERT INTO waiter VALUES
+('Molly','Webber','782058274','3');
+INSERT INTO waiter VALUES
+('Janis','Steward','640722173','3');
+INSERT INTO waiter VALUES
+('Rachel','Owens','422170027','3');
+INSERT INTO waiter VALUES
+('Elsa','Ball','247504857','3');
+INSERT INTO waiter VALUES
+('Louis','Lucas','119552762','3');
+INSERT INTO waiter VALUES
+('Adam','Lamb','908862655','3');
+INSERT INTO waiter VALUES
+('Brian','Jefferson','339712993','3');
+INSERT INTO waiter VALUES
+('Cindy','Nash','864687613','3');
+INSERT INTO waiter VALUES
+('Mack','Harrington','169774759','3');
+INSERT INTO waiter VALUES
+('Mae','Paul','239631814','3');
+INSERT INTO waiter VALUES
+('Brittany','Greer','966724744','3');
+INSERT INTO waiter VALUES
+('Glenn','Quinn','206686271','3');
+INSERT INTO waiter VALUES
+('Barbara','Kennedy','427615765','3');
+INSERT INTO waiter VALUES
+('Alexandar','Bass','604922236','3');
+INSERT INTO waiter VALUES
+('Nellie','Townsend','869555341','3');
+INSERT INTO waiter VALUES
+('Donna','Manning','944562445','3');
+INSERT INTO waiter VALUES
+('Stacey','Logan','470131226','3');
+INSERT INTO waiter VALUES
+('Jessie','Castro','787124279','4');
+INSERT INTO waiter VALUES
+('Ebony','Graham','143195609','4');
+INSERT INTO waiter VALUES
+('Santos','Davidson','735422678','4');
+INSERT INTO waiter VALUES
+('Roberto','Conner','248160825','4');
+INSERT INTO waiter VALUES
+('Lynda','Castro','332002918','4');
+INSERT INTO waiter VALUES
+('May','Jones','667055290','4');
+INSERT INTO waiter VALUES
+('Stephen','Gardner','529490318','4');
+INSERT INTO waiter VALUES
+('Rosemarie','Cortez','629607671','4');
+INSERT INTO waiter VALUES
+('Daryl','Bass','357891473','4');
+INSERT INTO waiter VALUES
+('Hugh','Blair','320420902','4');
+INSERT INTO waiter VALUES
+('Brandon','Knight','014231740','4');
+INSERT INTO waiter VALUES
+('Lee','Rodriguez','915100366','4');
+INSERT INTO waiter VALUES
+('Gladys','Douglas','696650160','4');
+INSERT INTO waiter VALUES
+('Myra','Carlson','162973570','4');
+INSERT INTO waiter VALUES
+('Nathaniel','Sullivan','137365552','4');
+INSERT INTO Waiter VALUES
+('Tommie','Dunn','388500976','4');
+INSERT INTO waiter VALUES
+('Alicia','Glover','329849378','4');
+INSERT INTO waiter VALUES
+('Mamie','Ramos','121695609','4');
+INSERT INTO waiter VALUES
+('Kim','O''brien','781882148','4');
+INSERT INTO waiter VALUES
+('Al','Pittman','475106015','4');
+INSERT INTO waiter VALUES
+('Lindsey','Wheeler','986513189','4');
+INSERT INTO waiter VALUES
+('Jeanne','Hines','075450752','4');
+INSERT INTO waiter VALUES
+('Brad','Montgomery','172019094','4');
+INSERT INTO waiter VALUES
+('Warren','Jacobs','459572139','4');
+INSERT INTO waiter VALUES
+('Laurie','Bishop','396829738','4');
+INSERT INTO waiter VALUES
+('Sherman','Frazier','476298405','4');
+INSERT INTO waiter VALUES
+('Erica','Thomas','781258251','4');
+INSERT INTO waiter VALUES
+('Annette','Berry','886180519','4');
+INSERT INTO waiter VALUES
+('Leonard','Maldonado','222258336','4');
+INSERT INTO waiter VALUES
+('Clay','Wise','453769941','4');
+
+INSERT INTO host VALUES
+('Joanne','Dawson','467774160','1');
+INSERT INTO host VALUES
+('Caroline','Divver','627134405','1');
+INSERT INTO host VALUES
+('Timmy','Dragonette','145752788','1');
+INSERT INTO host VALUES
+('Katie','Erskine','950280912','1');
+INSERT INTO host VALUES
+('Haley','Geier','247618164','1');
+INSERT INTO host VALUES
+('Wil','Ferrer','874298057','2');
+INSERT INTO host VALUES
+('Kim','Grinnell','399996036','2');
+INSERT INTO host VALUES
+('Brett','Walsh','007583953','2');
+INSERT INTO host VALUES
+('Stephen','Henderson','624008448','2');
+INSERT INTO host VALUES
+('Susan','McDonald','051002445','2');
+INSERT INTO host VALUES
+('Cory','Woo','242907735','3');
+INSERT INTO host VALUES
+('Rose','Vaughn','802094799','3');
+INSERT INTO host VALUES
+('Adam','Scott','171507888','3');
+INSERT INTO host VALUES
+('Kristen','Groves','041844151','3');
+INSERT INTO host VALUES
+('Matt','Herlihy','574121119','3');
+INSERT INTO host VALUES
+('Kathy','Johnson','265950240','4');
+INSERT INTO host VALUES
+('John','Duffy','438830169','4');
+INSERT INTO host VALUES
+('Allie','Margolis','853550567','4');
+INSERT INTO host VALUES
+('Annie','Maloney','558816253','4');
+INSERT INTO host VALUES
+('Connor','Lawrence','581172095','4');
