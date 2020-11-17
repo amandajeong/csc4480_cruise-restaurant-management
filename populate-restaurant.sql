26e04e6d318cd379e390b6e9422ac8619d6ee7e8
kkey1 <kkey1@villanova.edu>
2020-11-17 11:45:19 -0500

    data for restaurant and check constraing schema

--- populate-restaurant.sql
+++ populate-restaurant.sql
@@ -1,353 +1,353 @@
 INSERT INTO restaurant VALUES
-('150 Central Park','1','10:00am-12:00am','15');
+('150 Central Park',1,15);
 INSERT INTO restaurant VALUES
-('Chops Grille','2','10:00am-12:00am','25');
+('Chops Grille',2,25);
 INSERT INTO restaurant VALUES
-('Giovanni''s Table','3','10:00am-12:00am','20');
+('Giovanni''s Table',3,20);
 INSERT INTO restaurant VALUES
-('Izumi Hibachi ' ||chr(38)|| ' Sushi','4','10:00am-12:00am','15');
+('Izumi Hibachi ' ||chr(38)|| ' Sushi',4,15);
 
 INSERT INTO waiter VALUES
-('Salvatore','Brooks','610951626','1');
+('Salvatore','Brooks',610951626,1);
 INSERT INTO waiter VALUES
-('Blanca','Arnold','496443955','1');
+('Blanca','Arnold',496443955,1);
 INSERT INTO waiter VALUES
-('Andrew','Newton','106990635','1');
+('Andrew','Newton',106990635,1);
 INSERT INTO waiter VALUES
-('Levi','Watkins','234941254','1');
+('Levi','Watkins',234941254,1);
 INSERT INTO waiter VALUES
-('Daniel','Page','380628740','1');
+('Daniel','Page',380628740,1);
 INSERT INTO waiter VALUES
-('Gordon','Perez','649928414','1');
+('Gordon','Perez',649928414,1);
 INSERT INTO waiter VALUES
-('Dallas','Schwartz','113168875','1');
+('Dallas','Schwartz',113168875,1);
 INSERT INTO waiter VALUES
-('Marsha','Woods','910781920','1');
+('Marsha','Woods',910781920,1);
 INSERT INTO waiter VALUES
-('Bernadette','Sullivan','941373596','1');
+('Bernadette','Sullivan',941373596,1);
 INSERT INTO waiter VALUES
-('Georgia','Waters','738480675','1');
+('Georgia','Waters',738480675,1);
 INSERT INTO waiter VALUES
-('Connie','Shaw','376363455','1');
+('Connie','Shaw',376363455,1);
 INSERT INTO waiter VALUES
-('Gerald','Hart','566538004','1');
+('Gerald','Hart',566538004,1);
 INSERT INTO waiter VALUES
-('Inez','Fox','271547370','1');
+('Inez','Fox',271547370,1);
 INSERT INTO waiter VALUES
-('Patsy','Williamson','220355904','1');
+('Patsy','Williamson',220355904,1);
 INSERT INTO waiter VALUES
-('Luther','Stevenson','559086787','1');
+('Luther','Stevenson',559086787,1);
 INSERT INTO waiter VALUES
-('Martin','Peters','014279718','1');
+('Martin','Peters',114279718,1);
 INSERT INTO waiter VALUES
-('Yvonne','Walton','923191059','1');
+('Yvonne','Walton',923191059,1);
 INSERT INTO waiter VALUES
-('Brandy','Holloway','106877608','1');
+('Brandy','Holloway',106877608,1);
 INSERT INTO waiter VALUES
-('Santiago','Park','294149400','1');
+('Santiago','Park',294149400,1);
 INSERT INTO waiter VALUES
-('Irving','Gonzalez','177362271','1');
+('Irving','Gonzalez',177362271,1);
 INSERT INTO waiter VALUES
-('Monica','Fleming','644251857','1');
+('Monica','Fleming',644251857,1);
 INSERT INTO waiter VALUES
-('Bert','Klein','344735295','1');
+('Bert','Klein',344735295,1);
 INSERT INTO waiter VALUES
-('Sherman','Conner','936961688','1');
+('Sherman','Conner',936961688,1);
 INSERT INTO waiter VALUES
-('Joe','Kim','654615221','1');
+('Joe','Kim',654615221,1);
 INSERT INTO waiter VALUES
-('Angelina','Harrison','980478363','1');
+('Angelina','Harrison',980478363,1);
 INSERT INTO waiter VALUES
-('Matthew','Hines','530329460','1');
+('Matthew','Hines',530329460,1);
 INSERT INTO waiter VALUES
-('Tabitha','Spencer','823871363','1');
+('Tabitha','Spencer',823871363,1);
 INSERT INTO waiter VALUES
-('Sergio','Mccoy','497622936','1');
+('Sergio','Mccoy',497622936,1);
 INSERT INTO waiter VALUES
-('Adam','Ingram','553583730','1');
+('Adam','Ingram',553583730,1);
 INSERT INTO waiter VALUES
-('Ellen','Burke','637379803','1');
+('Ellen','Burke',637379803,1);
 INSERT INTO waiter VALUES
-('Edith','Tran','763097326','2');
+('Edith','Tran',763097326,2);
 INSERT INTO waiter VALUES
-('Arturo','Stevens','389790097','2');
+('Arturo','Stevens',389790097,2);
 INSERT INTO waiter VALUES
-('Glenda','Pierce','719187559','2');
+('Glenda','Pierce',719187559,2);
 INSERT INTO waiter VALUES
-('Darren','Weber','428866429','2');
+('Darren','Weber',428866429,2);
 INSERT INTO waiter VALUES
-('Oscar','Boyd','188923740','2');
+('Oscar','Boyd',188923740,2);
 INSERT INTO waiter VALUES
-('Melba','Wolfe','883516597','2');
+('Melba','Wolfe',883516597,2);
 INSERT INTO waiter VALUES
-('Otis','Gutierrez','755386053','2');
+('Otis','Gutierrez',755386053,2);
 INSERT INTO waiter VALUES
-('Marcus','Ward','389573576','2');
+('Marcus','Ward',389573576,2);
 INSERT INTO waiter VALUES
-('Marc','Paul','698275771','2');
+('Marc','Paul',698275771,2);
 INSERT INTO waiter VALUES
-('Archie','Crawford','702623399','2');
+('Archie','Crawford',702623399,2);
 INSERT INTO waiter VALUES
-('Anna','Francis','087510269','2');
+('Anna','Francis',187510269,2);
 INSERT INTO waiter VALUES
-('Salvador','Ortega','530847557','2');
+('Salvador','Ortega',530847557,2);
 INSERT INTO waiter VALUES
-('Regina','Freeman','848433641','2');
+('Regina','Freeman',848433641,2);
 INSERT INTO waiter VALUES
-('Gwen','Cook','392933517','2');
+('Gwen','Cook',392933517,2);
 INSERT INTO waiter VALUES
-('Roger','Wright','979450154','2');
+('Roger','Wright',979450154,2);
 INSERT INTO waiter VALUES
-('Garry','Nunez','099999774','2');
+('Garry','Nunez',999999774,2);
 INSERT INTO waiter VALUES
-('Melissa','Harrington','204922703','2');
+('Melissa','Harrington',204922703,2);
 INSERT INTO waiter VALUES
-('Krystal','Garza','403514862','2');
+('Krystal','Garza',403514862,2);
 INSERT INTO waiter VALUES
-('Troy','Powers','675138259','2');
+('Troy','Powers',675138259,2);
 INSERT INTO waiter VALUES
-('Emily','Lyons','373420391','2');
+('Emily','Lyons',373420391,2);
 INSERT INTO waiter VALUES
-('Tim','Davis','371778322','2');
+('Tim','Davis',371778322,2);
 INSERT INTO waiter VALUES
-('Lauren','Webster','497297008','2');
+('Lauren','Webster',497297008,2);
 INSERT INTO waiter VALUES
-('Kelly','Cook','045893726','2');
+('Kelly','Cook',145893726,2);
 INSERT INTO waiter VALUES
-('Tammy','King','302172621','2');
+('Tammy','King',302172621,2);
 INSERT INTO waiter VALUES
-('Tricia','Jacobs','375490159','2');
+('Tricia','Jacobs',375490159,2);
 INSERT INTO waiter VALUES
-('Tasha','Adkins','736501864','2');
+('Tasha','Adkins',736501864,2);
 INSERT INTO waiter VALUES
-('Nichole','Berry','621891684','2');
+('Nichole','Berry',621891684,2);
 INSERT INTO waiter VALUES
-('Simon','Morris','625754045','2');
+('Simon','Morris',625754045,2);
 INSERT INTO waiter VALUES
-('Eloise','Reyes','820527340','2');
+('Eloise','Reyes',820527340,2);
 INSERT INTO waiter VALUES
-('Karen','Maxwell','643942793','2');
+('Karen','Maxwell',643942793,2);
 INSERT INTO waiter VALUES
-('Bradford','Quinn','333238328','2');
+('Bradford','Quinn',333238328,2);
 INSERT INTO waiter VALUES
-('Ruby','Andrews','667728536','2');
+('Ruby','Andrews',667728536,2);
 INSERT INTO waiter VALUES
-('Suzanna','Lucas','561816142','2');
+('Suzanna','Lucas',561816142,2);
 INSERT INTO waiter VALUES
-('Alicia','Sanchez','678205626','2');
+('Alicia','Sanchez',678205626,2);
 INSERT INTO waiter VALUES
-('Madeline','Hicks','429622260','2');
+('Madeline','Hicks',429622260,2);
 INSERT INTO waiter VALUES
-('William','Park','599298802','2');
+('William','Park',599298802,2);
 INSERT INTO waiter VALUES
-('Carla','Snyder','520992111','2');
+('Carla','Snyder',520992111,2);
 INSERT INTO waiter VALUES
-('Michele','Hines','955722366','2');
+('Michele','Hines',955722366,2);
 INSERT INTO waiter VALUES
-('Tonya','Hudson','566362337','2');
+('Tonya','Hudson',566362337,2);
 INSERT INTO waiter VALUES
-('Virginia','Peterson','141794252','2');
+('Virginia','Peterson',141794252,2);
 INSERT INTO waiter VALUES
-('Dianna','Swanson','265545313','2');
+('Dianna','Swanson',265545313,2);
 INSERT INTO waiter VALUES
-('Maggie','Miller','981219857','2');
+('Maggie','Miller',981219857,2);
 INSERT INTO waiter VALUES
-('Alexander','Copeland','809229921','2');
+('Alexander','Copeland',809229921,2);
 INSERT INTO waiter VALUES
-('Megan','Burgess','941452922','2');
+('Megan','Burgess',941452922,2);
 INSERT INTO waiter VALUES
-('Lynn','Tucker','518209593','2');
+('Lynn','Tucker',518209593,2);
 INSERT INTO waiter VALUES
-('Tracy','Washington','275631721','2');
+('Tracy','Washington',275631721,2);
 INSERT INTO waiter VALUES
-('Roman','Stevenson','384098131','2');
+('Roman','Stevenson',384098131,2);
 INSERT INTO waiter VALUES
-('Margarita','Lopez','586524454','2');
+('Margarita','Lopez',586524454,2);
 INSERT INTO waiter VALUES
-('Alberto','Kennedy','696383921','2');
+('Alberto','Kennedy',696383921,2);
 INSERT INTO waiter VALUES
-('Grant','Fernandez','654098240','2');
+('Grant','Fernandez',654098240,2);
 INSERT INTO waiter VALUES
-('Arnold','Love','990653793','3');
+('Arnold','Love',990653793,3);
 INSERT INTO waiter VALUES
-('Wayne','Estrada','435285477','3');
+('Wayne','Estrada',435285477,3);
 INSERT INTO waiter VALUES
-('Shawna','Carroll','486540843','3');
+('Shawna','Carroll',486540843,3);
 INSERT INTO waiter VALUES
-('May','Tate','007322512','3');
+('May','Tate',117322512,3);
 INSERT INTO waiter VALUES
-('Winifred','Coleman','367478363','3');
+('Winifred','Coleman',367478363,3);
 INSERT INTO waiter VALUES
-('Ricardo','Vargas','199143524','3');
+('Ricardo','Vargas',199143524,3);
 INSERT INTO waiter VALUES
-('Pam','Walters','801656354','3');
+('Pam','Walters',801656354,3);
 INSERT INTO waiter VALUES
-('Lucille','Wallace','576780663','3');
+('Lucille','Wallace',576780663,3);
 INSERT INTO waiter VALUES
-('Stephen','Nichols','580038904','3');
+('Stephen','Nichols',580038904,3);
 INSERT INTO waiter VALUES
-('Jenna','Wolfe','740241587','3');
+('Jenna','Wolfe',740241587,3);
 INSERT INTO waiter VALUES
-('Francisco','Castillo','278269343','3');
+('Francisco','Castillo',278269343,3);
 INSERT INTO waiter VALUES
-('Johnny','Alvarez','611368229','3');
+('Johnny','Alvarez',611368229,3);
 INSERT INTO waiter VALUES
-('Kellie','Erickson','300221215','3');
+('Kellie','Erickson',300221215,3);
 INSERT INTO waiter VALUES
-('Wilson','Bush','270318331','3');
+('Wilson','Bush',270318331,3);
 INSERT INTO waiter VALUES
-('Johanna','Potter','402123184','3');
+('Johanna','Potter',402123184,3);
 INSERT INTO waiter VALUES
-('Christine','Brooks','229528710','3');
+('Christine','Brooks',229528710,3);
 INSERT INTO waiter VALUES
-('Bryant','Gregory','276121294','3');
+('Bryant','Gregory',276121294,3);
 INSERT INTO waiter VALUES
-('Ricky','Patton','593510178','3');
+('Ricky','Patton',593510178,3);
 INSERT INTO waiter VALUES
-('Nathan','Brewer','308672638','3');
+('Nathan','Brewer',308672638,3);
 INSERT INTO waiter VALUES
-('Elijah','Hicks','580605218','3');
+('Elijah','Hicks',580605218,3);
 INSERT INTO waiter VALUES
-('Bennie','Hudson','790896292','3');
+('Bennie','Hudson',790896292,3);
 INSERT INTO waiter VALUES
-('Irvin','Vega','539062233','3');
+('Irvin','Vega',539062233,3);
 INSERT INTO waiter VALUES
-('Malcolm','Barrett','838248760','3');
+('Malcolm','Barrett',838248760,3);
 INSERT INTO waiter VALUES
-('Molly','Webber','782058274','3');
+('Molly','Webber',782058274,3);
 INSERT INTO waiter VALUES
-('Janis','Steward','640722173','3');
+('Janis','Steward',640722173,3);
 INSERT INTO waiter VALUES
-('Rachel','Owens','422170027','3');
+('Rachel','Owens',422170027,3);
 INSERT INTO waiter VALUES
-('Elsa','Ball','247504857','3');
+('Elsa','Ball',247504857,3);
 INSERT INTO waiter VALUES
-('Louis','Lucas','119552762','3');
+('Louis','Lucas',119552762,3);
 INSERT INTO waiter VALUES
-('Adam','Lamb','908862655','3');
+('Adam','Lamb',908862655,3);
 INSERT INTO waiter VALUES
-('Brian','Jefferson','339712993','3');
+('Brian','Jefferson',339712993,3);
 INSERT INTO waiter VALUES
-('Cindy','Nash','864687613','3');
+('Cindy','Nash',864687613,3);
 INSERT INTO waiter VALUES
-('Mack','Harrington','169774759','3');
+('Mack','Harrington',169774759,3);
 INSERT INTO waiter VALUES
-('Mae','Paul','239631814','3');
+('Mae','Paul',239631814,3);
 INSERT INTO waiter VALUES
-('Brittany','Greer','966724744','3');
+('Brittany','Greer',966724744,3);
 INSERT INTO waiter VALUES
-('Glenn','Quinn','206686271','3');
+('Glenn','Quinn',206686271,3);
 INSERT INTO waiter VALUES
-('Barbara','Kennedy','427615765','3');
+('Barbara','Kennedy',427615765,3);
 INSERT INTO waiter VALUES
-('Alexandar','Bass','604922236','3');
+('Alexandar','Bass',604922236,3);
 INSERT INTO waiter VALUES
-('Nellie','Townsend','869555341','3');
+('Nellie','Townsend',869555341,3);
 INSERT INTO waiter VALUES
-('Donna','Manning','944562445','3');
+('Donna','Manning',944562445,3);
 INSERT INTO waiter VALUES
-('Stacey','Logan','470131226','3');
+('Stacey','Logan',470131226,3);
 INSERT INTO waiter VALUES
-('Jessie','Castro','787124279','4');
+('Jessie','Castro',787124279,4);
 INSERT INTO waiter VALUES
-('Ebony','Graham','143195609','4');
+('Ebony','Graham',143195609,4);
 INSERT INTO waiter VALUES
-('Santos','Davidson','735422678','4');
+('Santos','Davidson',735422678,4);
 INSERT INTO waiter VALUES
-('Roberto','Conner','248160825','4');
+('Roberto','Conner',248160825,4);
 INSERT INTO waiter VALUES
-('Lynda','Castro','332002918','4');
+('Lynda','Castro',332002918,4);
 INSERT INTO waiter VALUES
-('May','Jones','667055290','4');
+('May','Jones',667055290,4);
 INSERT INTO waiter VALUES
-('Stephen','Gardner','529490318','4');
+('Stephen','Gardner',529490318,4);
 INSERT INTO waiter VALUES
-('Rosemarie','Cortez','629607671','4');
+('Rosemarie','Cortez',629607671,4);
 INSERT INTO waiter VALUES
-('Daryl','Bass','357891473','4');
+('Daryl','Bass',357891473,4);
 INSERT INTO waiter VALUES
-('Hugh','Blair','320420902','4');
+('Hugh','Blair',320420902,4);
 INSERT INTO waiter VALUES
-('Brandon','Knight','014231740','4');
+('Brandon','Knight',114231740,4);
 INSERT INTO waiter VALUES
-('Lee','Rodriguez','915100366','4');
+('Lee','Rodriguez',915100366,4);
 INSERT INTO waiter VALUES
-('Gladys','Douglas','696650160','4');
+('Gladys','Douglas',696650160,4);
 INSERT INTO waiter VALUES
-('Myra','Carlson','162973570','4');
+('Myra','Carlson',162973570,4);
 INSERT INTO waiter VALUES
-('Nathaniel','Sullivan','137365552','4');
+('Nathaniel','Sullivan',137365552,4);
 INSERT INTO Waiter VALUES
-('Tommie','Dunn','388500976','4');
+('Tommie','Dunn',388500976,4);
 INSERT INTO waiter VALUES
-('Alicia','Glover','329849378','4');
+('Alicia','Glover',329849378,4);
 INSERT INTO waiter VALUES
-('Mamie','Ramos','121695609','4');
+('Mamie','Ramos',121695609,4);
 INSERT INTO waiter VALUES
-('Kim','O''brien','781882148','4');
+('Kim','O''brien',781882148,4);
 INSERT INTO waiter VALUES
-('Al','Pittman','475106015','4');
+('Al','Pittman',475106015,4);
 INSERT INTO waiter VALUES
-('Lindsey','Wheeler','986513189','4');
+('Lindsey','Wheeler',986513189,4);
 INSERT INTO waiter VALUES
-('Jeanne','Hines','075450752','4');
+('Jeanne','Hines',175450752,4);
 INSERT INTO waiter VALUES
-('Brad','Montgomery','172019094','4');
+('Brad','Montgomery',172019094,4);
 INSERT INTO waiter VALUES
-('Warren','Jacobs','459572139','4');
+('Warren','Jacobs',459572139,4);
 INSERT INTO waiter VALUES
-('Laurie','Bishop','396829738','4');
+('Laurie','Bishop',396829738,4);
 INSERT INTO waiter VALUES
-('Sherman','Frazier','476298405','4');
+('Sherman','Frazier',476298405,4);
 INSERT INTO waiter VALUES
-('Erica','Thomas','781258251','4');
+('Erica','Thomas',781258251,4);
 INSERT INTO waiter VALUES
-('Annette','Berry','886180519','4');
+('Annette','Berry',886180519,4);
 INSERT INTO waiter VALUES
-('Leonard','Maldonado','222258336','4');
+('Leonard','Maldonado',222258336,4);
 INSERT INTO waiter VALUES
-('Clay','Wise','453769941','4');
+('Clay','Wise',453769941,4);
 
 INSERT INTO host VALUES
-('Joanne','Dawson','467774160','1');
+('Joanne','Dawson',467774160,1);
 INSERT INTO host VALUES
-('Caroline','Divver','627134405','1');
+('Caroline','Divver',627134405,1);
 INSERT INTO host VALUES
-('Timmy','Dragonette','145752788','1');
+('Timmy','Dragonette',145752788,1);
 INSERT INTO host VALUES
-('Katie','Erskine','950280912','1');
+('Katie','Erskine',950280912,1);
 INSERT INTO host VALUES
-('Haley','Geier','247618164','1');
+('Haley','Geier',247618164,1);
 INSERT INTO host VALUES
-('Wil','Ferrer','874298057','2');
+('Wil','Ferrer',874298057,2);
 INSERT INTO host VALUES
-('Kim','Grinnell','399996036','2');
+('Kim','Grinnell',399996036,2);
 INSERT INTO host VALUES
-('Brett','Walsh','007583953','2');
+('Brett','Walsh',117583953,2);
 INSERT INTO host VALUES
-('Stephen','Henderson','624008448','2');
+('Stephen','Henderson',624008448,2);
 INSERT INTO host VALUES
-('Susan','McDonald','051002445','2');
+('Susan','McDonald',151002445,2);
 INSERT INTO host VALUES
-('Cory','Woo','242907735','3');
+('Cory','Woo',242907735,3);
 INSERT INTO host VALUES
-('Rose','Vaughn','802094799','3');
+('Rose','Vaughn',802094799,3);
 INSERT INTO host VALUES
-('Adam','Scott','171507888','3');
+('Adam','Scott',171507888,3);
 INSERT INTO host VALUES
-('Kristen','Groves','041844151','3');
+('Kristen','Groves',041844151,3);
 INSERT INTO host VALUES
-('Matt','Herlihy','574121119','3');
+('Matt','Herlihy',574121119,3);
 INSERT INTO host VALUES
-('Kathy','Johnson','265950240','4');
+('Kathy','Johnson',265950240,4);
 INSERT INTO host VALUES
-('John','Duffy','438830169','4');
+('John','Duffy',438830169,4);
 INSERT INTO host VALUES
-('Allie','Margolis','853550567','4');
+('Allie','Margolis',853550567,4);
 INSERT INTO host VALUES
-('Annie','Maloney','558816253','4');
+('Annie','Maloney',558816253,4);
 INSERT INTO host VALUES
-('Connor','Lawrence','581172095','4');
+('Connor','Lawrence',581172095,4);
 
 INSERT INTO menu (Mrest_id, Menu_id, Mitem_name, Mcost)
 -- 1 - 150 Central Park
@@ -455,3 +455,42 @@ INSERT INTO menu (Mrest_id, Menu_id, Mitem_name, Mcost)
  SELECT 4, 431, 'Tori Teppan Classic', 39.00 FROM dual UNION ALL
  SELECT 4, 432, 'Gyu Teppan Classic', 39.00 FROM dual UNION ALL
  SELECT 4, 433, 'Ebi Teppan Classic', 39.00 FROM dual ;
+ 
+INSERT INTO reservation VALUES
+(1001,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),4,610951626,467774160);
+INSERT INTO reservation VALUES
+(1002,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),5,496443955,627134405);
+INSERT INTO reservation VALUES
+(1003,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),3,106990635,145752788);
+INSERT INTO reservation VALUES
+(1004,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),3,234941254,950280912);
+INSERT INTO reservation VALUES
+(1005,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),4,380628740,247618164);
+INSERT INTO reservation VALUES
+(1006,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),5,649928414,467774160);
+INSERT INTO reservation VALUES
+(1007,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),3,496443955,627134405);
+INSERT INTO reservation VALUES
+(1008,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),5,106990635,145752788);
+INSERT INTO reservation VALUES
+(1009,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),4,234941254,950280912);
+INSERT INTO reservation VALUES
+(1010,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),5,380628740,247618164);
+INSERT INTO reservation VALUES
+(1011,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),3,610951626,467774160);
+INSERT INTO reservation VALUES
+(1012,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),4,496443955,627134405);
+INSERT INTO reservation VALUES
+(1013,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),3,106990635,145752788);
+INSERT INTO reservation VALUES
+(1014,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),5,234941254,950280912);
+INSERT INTO reservation VALUES
+(1015,1,TO_DATE('2021-01-01 18:00','YYYY-MM-DD HH24:MI'),4,380628740,247618164);
+INSERT INTO reservation VALUES
+(2001,2,TO_DATE('2020-03-09 12:00', 'YYYY-MM-DD HH24:MI'),7,763097326,874298057);
+INSERT INTO reservation VALUES
+(2002,2,TO_DATE('2020-04-09 14:00', 'YYYY-MM-DD HH24:MI'),8,389790097,399996036);
+INSERT INTO reservation VALUES
+(2003,2,TO_DATE('2020-05-09 16:00', 'YYYY-MM-DD HH24:MI'),8,719187559,117583953);
+INSERT INTO reservation VALUES
+(2004,2,TO_DATE('2021-01-01 10:00', 'YYYY-MM-DD HH24:MI'),4,719187559,117583953);
--- restaurant-schema.sql
+++ restaurant-schema.sql
@@ -2,7 +2,6 @@ DROP TABLE restaurant CASCADE CONSTRAINTS;
 CREATE TABLE restaurant (
     rest_name   varchar2(50)    not null,
     rest_id     number(4)       not null,
-    hours       char(15)        not null, --format: 10:00am-12:00am
     seat_cap    number(4)       not null,
     primary key (rest_id)
 );
@@ -11,7 +10,7 @@ DROP TABLE waiter CASCADE CONSTRAINTS;
 CREATE TABLE waiter (
     Wfname      varchar2(25)    not null,
     Wlname      varchar2(25)    not null,
-    Wssn        char(9)         not null,
+    Wssn        number(9)         not null,
     Wrest_id    number(4)       not null,
     primary key (Wssn),
     foreign key (Wrest_id) references restaurant(rest_id)
@@ -21,7 +20,7 @@ DROP TABLE host CASCADE CONSTRAINTS;
 CREATE TABLE host (
     Hfname      varchar2(25)    not null,
     Hlname      varchar2(25)    not null,
-    Hssn        char(9)         not null,
+    Hssn        number(9)         not null,
     Hrest_id    number(4)       not null,
     primary key (Hssn),
     foreign key (Hrest_id) references restaurant(rest_id)
@@ -41,11 +40,10 @@ DROP TABLE reservation CASCADE CONSTRAINTS;
 CREATE TABLE reservation (
     Reservation_id  number(9)   not null,
     Rrest_id        number(4)   not null,
-    Rdate           date        not null,
-    Rtime           char(15)    not null,
-    Rgroup_size     number(2)   not null,
-    Rwaiter_ssn     char(9)     not null,
-    Rhost_ssn       char(9)     not null,
+    Rdatetime       date        not null,
+    Rgroup_size     number(2)   not null check (Rgroup_size < 9),
+    Rwaiter_ssn     number(9)     not null,
+    Rhost_ssn       number(9)     not null,
     primary key (Reservation_id),
     foreign key (Rrest_id) references restaurant(rest_id),
     foreign key (Rwaiter_ssn) references waiter(Wssn),
